23fb62e8ea1ec698c62bce2bbabd49b0fc4a0d37
merrysutijono <68658448+merrysutijono@users.noreply.github.com>
2025-06-09 14:21:58 -0700

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,13 +3,13 @@ import code from '../assets/code2.png';
 
 const Works = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-black bg-[#cbc5c3]'>
+    <div name='works' className='w-full md:h-screen text-black bg-[#cbc5c3]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8 w-full flex justify-center items-center flex-col'>
           <p className='text-4xl font-bold inline border-b-4 text-black border-white'>
             Work
           </p>
-          <p className='py-6 text-2xl'>Here are some of my most recent work...</p>
+          <p className='py-6 text-2xl'>Here are some of my most recent projects...</p>
         </div>
 {/* Container */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
