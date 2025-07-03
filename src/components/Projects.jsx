3def8ff9e81a44231fb62c33776f1ec6b7184316
Merry Sutijono <68658448+merrysutijono@users.noreply.github.com>
2025-07-03 13:34:43 -0700

    Update and rename Works.jsx to Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import code from '../assets/code2.png';
+import code from '../assets/.png';
 
-const Works = () => {
+const Projects = () => {
   return (
-    <div name='works' className='w-full md:h-screen text-black bg-[#cbc5c3]'>
+    <div name='projects' className='w-full md:h-screen text-black bg-[#cbc5c3]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8 w-full flex justify-center items-center flex-col'>
           <p className='text-4xl font-bold inline border-b-4 text-black border-white'>
-            Work
+            Project
           </p>
           <p className='py-6 text-2xl'>Here are some of my most recent projects...</p>
         </div>
@@ -44,4 +44,4 @@ const Works = () => {
     </div>
   );
 };
-export default Works;
+export default Projects;
